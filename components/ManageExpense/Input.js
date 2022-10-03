895209b6ecbc2c093c14ad372c8d81b9ec601d8d
oguz yurken <oguz.yurken@gmail.com>
2022-10-03 23:48:41 +0300

    Create Input component

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,55 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { GlobalStyles } from "../../constants/styles";
+
+const Input = ({ label, style, invalid, textInputConfig }) => {
+  const inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
+  }
+
+  return (
+    <View style={[styles.inputContainer, style]}>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>
+        {label}
+      </Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    color: GlobalStyles.colors.primary700,
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50,
+  },
+});
